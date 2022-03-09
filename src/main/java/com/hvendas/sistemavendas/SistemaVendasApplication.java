790fc211d09cb6f11f2d6d05bf41a21f42516de8
Ricsoon <painonex2@gmail.com>
2022-03-09 16:25:22 -0300

    Projeto criado e primeiras migrations

---
+++ src/main/java/com/hvendas/sistemavendas/SistemaVendasApplication.java
@@ -0,0 +1,14 @@
+package com.hvendas.sistemavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendasApplication.class, args);
+		//envio
+	}
+
+}
