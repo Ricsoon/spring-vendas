84555e4c54f05a72a559348e4dfb1033247bf5a9
Ricsoon <painonex2@gmail.com>
2022-03-15 13:20:48 -0300

    Ajuste na execução do código - especificando pacotes

--- src/main/java/com/hvendas/sistemavendas/SistemaVendasApplication.java
+++ src/main/java/com/hvendas/sistemavendas/SistemaVendasApplication.java
@@ -2,7 +2,13 @@ package com.hvendas.sistemavendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"com.hvendas.sistemavendas.entities"})
+@EnableJpaRepositories(basePackages = {"com.hvendas.sistemavendas.repository"})
+@ComponentScan(basePackages = {"com.hvendas.sistemavendas.service", "com.hvendas.sistemavendas.controller"})
 @SpringBootApplication
 public class SistemaVendasApplication {
 
